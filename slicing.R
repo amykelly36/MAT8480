d1753cfa48bef8c945ad9358a8bfefd74f95f7ba
amykelly36 <34772616+amykelly36@users.noreply.github.com>
2019-08-29 13:03:28 -0400

    Add files via upload

---
+++ slicing.R
@@ -0,0 +1,38 @@
+# import data
+data<-read.csv("pva97nko.csv")
+
+## RCookbook(p112)
+# select a value
+data[1,"TargetB"]
+
+# select multiple rows
+demo<-data[1:20,]
+
+# select multiple columns by name using c()
+demo[,c("TargetB", "GiftCnt36", "GiftCntAll")]
+demo[c("TargetB", "GiftCnt36", "GiftCntAll")]
+
+# select multiple columns by numeric indices
+demo[1:3]
+demo[c(1,2,3)]
+demo[-c(1,3)]
+
+# subset data with row and column indices
+demo[1:10, c("TargetB", "GiftCnt36")]
+
+# set conditions to filter data
+demo[demo$DemGender=="F",1:5]
+demo[demo$GiftCnt36<2, 1:5]
+
+# which function returns the indexes of satisfied data
+which(demo$DemGender=="F")
+
+
+# logical operators
+# == means ‘is equal to’
+# != means ‘is not equal to’
+# < means ` is less than’
+# > means ` is greater than’
+# <= means ` is less than or equal to’
+# >= means ` is greater than or equal to’
+
